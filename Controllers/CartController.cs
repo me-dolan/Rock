29d897672f7bb813920f6e4c233302abc021d2ac
user <user@DOLAN-PC>
2022-05-02 15:44:17 +0700

    Добавьте файлы проекта.

---
+++ Controllers/CartController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Rock.Data;
+using Rock.Models;
+using Rock.Models.ViewModels;
+using Rock.Utility;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+namespace Rock.Controllers
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        [BindProperty]
+        public ProductUserVM ProductUserVM { get; set; }
+
+        public CartController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<ShoppingCart> shoppingCarts = new List<ShoppingCart>();
+            if(HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WConst.SessionCart) !=null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WConst.SessionCart).Count() > 0)
+            {
+                shoppingCarts = HttpContext.Session.Get<List<ShoppingCart>>(WConst.SessionCart);
+            }
+
+            List<int> productInCart = shoppingCarts.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> productList = _context.Products.Where(u => productInCart.Contains(u.Id));
+
+            return View(productList);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+            return RedirectToAction(nameof(Summary));
+        }
+
+        public IActionResult Summary()
+        {
+            //var claimsIdentity = (ClaimsIdentity)User.Identity;
+            //var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            var userId = User.FindFirstValue(ClaimTypes.Name);
+
+            List<ShoppingCart> shoppingCarts = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WConst.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WConst.SessionCart).Count() > 0)
+            {
+                shoppingCarts = HttpContext.Session.Get<List<ShoppingCart>>(WConst.SessionCart);
+            }
+
+            List<int> productInCart = shoppingCarts.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> productList = _context.Products.Where(u => productInCart.Contains(u.Id));
+
+            ProductUserVM = new ProductUserVM()
+            {
+                ApplicationUser = _context.ApplicationUsers.FirstOrDefault(u => u.Id == userId),
+                ProductList = productList.ToList()
+            };
+
+            return View(ProductUserVM);
+        }
+
+        public IActionResult Remove(int id)
+        {
+            List<ShoppingCart> shoppingCarts = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WConst.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WConst.SessionCart).Count() > 0)
+            {
+                shoppingCarts = HttpContext.Session.Get<List<ShoppingCart>>(WConst.SessionCart);
+            }
+
+            shoppingCarts.Remove(shoppingCarts.FirstOrDefault(i => i.ProductId == id));
+
+            List<int> productInCart = shoppingCarts.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> productList = _context.Products.Where(u => productInCart.Contains(u.Id));
+
+            HttpContext.Session.Set(WConst.SessionCart, shoppingCarts);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Utility/EmailSender.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Threading.Tasks;
+using System;
+using MailKit.Net.Smtp;
+using MimeKit;
+using MailKit;
+
+namespace Rock.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            MimeMessage emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("", ""));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = htmlMessage
+            };
+
+            using (SmtpClient client = new SmtpClient())
+            {
+                await client.ConnectAsync("", 25, false);
+                await client.AuthenticateAsync("", "");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
